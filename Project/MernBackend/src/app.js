14b1f7e3f1cd7fcc80ef5cdbe19c2999d25e1d69
202001188 <202001188@daiict.ac.in>
2023-03-25 14:05:02 +0530

    Feature: Added responsive home page

--- Project/MernBackend/src/app.js
+++ Project/MernBackend/src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const hbs = require("hbs");
+const ejs = require("ejs");
 
 require("./db/conn");
 
@@ -12,19 +12,19 @@ const templates_path = path.join(__dirname , "../templates/views");
 const partials_path = path.join(__dirname , "../templates/partials");
 
 app.use(express.static(static_path))
-app.set("view engine", "hbs");
+app.set("view engine", "ejs");
 app.set("views", templates_path);
-hbs.registerPartials(partials_path);
+// hbs.registerPartials(partials_path);
 
 app.get("/", (req, res) => {
     //res.send("Hello and welcome to my page")
     res.render("index")
 });
 
-app.get("/login", (req, res) => {
-    //res.send("Hello and welcome to my page")
-    res.render("login")
-});
+// app.get("/login", (req, res) => {
+//     //res.send("Hello and welcome to my page")
+//     res.render("login")
+// });
 
 // app.get("/tables", (req, res) => {
 //     //res.send("Hello and welcome to my page")
