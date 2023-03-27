ea077e940244961faf90dfb95b472fe7cd467540
vyompatel <vyompatel@vyompatels-MacBook-Air.local>
2023-03-27 22:14:27 +0530

    StudentDashboard updated

--- app.js
+++ app.js
@@ -5,8 +5,29 @@ const bodyParser = require('body-parser');
 const port = process.env.port || 9000
 const app = express();
 
-app.get('/',(err,res)=>{
-    res.send("hh")
-})
-app.listen(port)
 
+app.set('view engine', 'ejs');//for ejs 
+
+app.use(express.static("public"));//look static file in public folder
+
+app.get('/', function(req, res) {
+    res.render('index', { });
+});
+app.get('/Studentlogin', function(req, res) {
+    res.render('Studentlogin', { });
+});
+app.get('/Adminlogin', function(req, res) {
+    res.render('Adminlogin', { });
+});
+app.get('/Facultylogin', function(req, res) {
+    res.render('Facultylogin', { });
+});
+app.get('/support', function(req, res) {
+    res.render('support', { });
+});
+app.listen(3000, function (req, res) {
+    console.log("Server is running on port number 3000");
+});
+app.get('/studentdash', function(req, res) {
+    res.render('Studentdashboard', { });
+});
